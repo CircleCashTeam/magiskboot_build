f5f6a2409a46617645fc138fa80b480b41a13e59
affggh <879632264@qq.com>
2024-12-04 03:40:44 +0800

    libc-compat: Add getpagesize compat function

---
+++ src/libc-compat/winsup/getpagesize.c
@@ -0,0 +1,21 @@
+/*
+ * Look like this maybe only used in boot.cpp at magiskboot
+ * So we just use a very simple way to get system pagesize 
+ * on windows.
+ */
+
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#include <stdint.h>
+
+#include "../include/winsup/getpagesize_compat.h"
+
+/*
+ * I hope this will work on windows
+ */
+static inline int32_t getpagesize() {
+    SYSTEM_INFO systemInfo;
+    GetSystemInfo(&systemInfo);
+
+    return systemInfo.dwPageSize;
+}
