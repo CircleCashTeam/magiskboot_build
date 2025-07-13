f91e30ac423cd1dcef0e5e6f720a8d68fb4021b7
affggh <879632264@qq.com>
2025-07-13 23:09:41 +0800

    update patches and build rule

--- scripts/clone_submodules.sh
+++ scripts/clone_submodules.sh
@@ -6,5 +6,5 @@ set -e
 set -x
 git submodule update --init --depth=1 --single-branch -j $(nproc) "$@" \
     src/{Magisk,android_{core,fmtlib,libbase,logging},corrosion,mman-win32,msvc_getline}/
-git -C src/Magisk submodule update --init --depth=1 --single-branch -j $(nproc) "$@" native/src/external/{cxx-rs,zopfli}/
+git -C src/Magisk submodule update --init --depth=1 --single-branch -j $(nproc) "$@" native/src/external/cxx-rs/
 set +x
